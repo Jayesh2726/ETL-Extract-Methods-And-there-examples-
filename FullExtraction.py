50e629e7755ed0a118acf6a826146b7db815e22c
Jayesh Magare <141212191+Jayesh2726@users.noreply.github.com>
2025-08-02 17:42:30 +0530

    Update FullExtraction.py

--- FullExtraction.py
+++ FullExtraction.py
@@ -2,17 +2,21 @@
 import pandas as js
 import sqlalchemy as sa
 
-username = 'postgres'               # replace with your username
-password = '846833'          # replace with your password
+# Database Connection with PostgreSQL
+username = 'Your Username'            
+password = 'Your Password'         
 host = 'localhost'
 port = '5432'
 database = 'DWH'  
 
+# Extract the source data
 df = js.read_csv("E:\\ETL Master\\employee_data01.csv")
-
+# Transform the data
 df ["Full Name"] = df["FirstName"] + " " + df["LastName"]
 df ["Full Name"] = df["Full Name"].str.upper()
 print(df)
 
+#Load data in PostgreSQL
 engine = sa.create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}')
+
 df.to_sql("Employee01",con=engine, index=False, if_exists="fail")
